e01b98d7ad96994d31fce26a3dacac35a2a411f9
rayan <eng.rayanhnide98@gmail.com>
2023-12-30 14:24:05 +0200

    first commit

---
+++ src/assets/lib/data.tsx
@@ -0,0 +1,661 @@
+import adobexdicon from "../../assets/icons/adobexdicon.svg";
+import figmaicon from "../../assets/icons/figmaicon.svg";
+import bootstarpicon from "../../assets/icons/Untitled.png";
+import htmlicon from "../../assets/icons/htmlicon.svg";
+import cssicon from "../../assets/icons/cssicon.svg";
+import reduxixon from "../../assets/icons/Redux.png";
+
+
+import javascripticon from "../../assets/icons/javascripticon.svg";
+import typescripticon from "../../assets/icons/typescripticon.svg";
+import reacticon from "../../assets/icons/reacticon.svg";
+import nextjsicon from "../../assets/icons/nextjsicon.svg";
+import nodejsicon from "../../assets/icons/nodejsicon.svg";
+import tailwindcssicon from "../../assets/icons/tailwindicon.svg";
+import mongodbicon from "../../assets/icons/mongoicon.svg";
+import sassscssicon from "../../assets/icons/sassicon.svg";
+
+
+import axiosicon from "../../assets/icons/axiosicon.svg";
+
+import webflowicon from "../../assets/icons/webflowicon.svg";
+import tastyMockup from "../../assets/img/startBootstrap.png";
+import crud from "../../assets/img/crud.png";
+import instorMockup from "../../assets/img/Reve.png";
+import silentmoonMockup from "../../assets/img/xcore.png";
+import Day from "../../assets/img/DayProject.png";
+import Global_school from "../../assets/img/gobal_school.png";
+import profilepicture from "../img/rayan.jpeg";
+import caricon from "../../assets/icons/car-icon.svg";
+import travelicon from "../../assets/icons/travel-icon.svg";
+import hardwareicon from "../../assets/icons/hardware-icon.svg";
+import nextjsiconwhite from "../../assets/icons/nextjsiconwhite.svg";
+
+import { GoHome, GoPerson, GoMail, GoStack, GoProject } from "react-icons/go";
+import { FiGithub, FiLink, FiLinkedin, FiMail } from "react-icons/fi";
+import Imprint from "../../components/Imprint";
+import Privacy from "../../components/Privacy";
+
+export const headerIntroData = {
+  title: {
+    de: "مرحبا أنا ريان هنيدي",
+    en: "Hi, I'm Rayan Hnide",
+  },
+  subtitle: "Front-End Web Development ",
+  description: {
+    de:"أنا ريان، مطور واجهة أمامية    أهدف الى تطوير مسيرتي المهنية والمشاركة في المشاريع الملهمة. أعرض هنا عملي وشغفي بتطوير الويب. دعونا نشكل الحلول الرقمية معًا ونشكل ",
+     en: "I'm Rayan, a Front-End Web developer with the goal of advancing my career and participating in inspiring projects. Here I present my work and my passion for web development. Let's shape digital solutions together and shape the future!",
+  },
+  buttons: [
+    {
+      name: "Contact",
+      label: {
+        de: "تواصل معي",
+        en: "Contact me",
+      },
+      icon: FiMail,
+      color: "main-btn",
+    },
+    {
+      name: "Projects",
+      label: {
+        de: "مشاريعي",
+        en: "My Projects",
+      },
+      icon: FiGithub,
+      color: "secondary-btn",
+    },
+  ],
+  profilepicture: profilepicture,
+} as const;
+
+export const projectsData = [
+  {
+    title: "XCore Corporation",
+    description:'',
+    description_EN:'',
+    technologies: [
+      { name: "Html", icon: htmlicon },
+      { name: "CSS", icon: cssicon },
+      { name: "JavaScript", icon: javascripticon },
+      { name: "Bootstrap", icon: bootstarpicon },
+      { name: "React", icon: reacticon },
+      { name: "Axios", icon: axiosicon },
+      { name: "MongoDB", icon: mongodbicon },
+      { name: "Node.js", icon: nodejsicon },
+      { name: "Figma", icon: figmaicon },
+    ],
+    image: silentmoonMockup,
+    deploymenturl: "http://www.xcore-net.com/",
+
+    githubicon: FiGithub,
+    deploymenticon: FiLink,
+    colors: {
+      main: "main-btn",
+      second: "secondary-btn",
+      icon: "white",
+      projectcolor: "#FFD5BD",
+    },
+  },
+  {
+    title: " Virtual Global Syrian School",
+    description:'',
+    description_EN:'',
+    technologies: [
+      { name: "Html", icon: htmlicon },
+      { name: "CSS", icon: cssicon },
+      { name: "JavaScript", icon: javascripticon },
+      { name: "Bootstrap", icon: bootstarpicon },
+      { name: "React", icon: reacticon },
+      { name: "Axios", icon: axiosicon },
+      { name: "MongoDB", icon: mongodbicon },
+      { name: "Node.js", icon: nodejsicon },
+      { name: "Figma", icon: figmaicon },
+    ],
+    image: Global_school,
+    deploymenturl: "https://www.global-school.net/",
+
+    githubicon: FiGithub,
+    deploymenticon: FiLink,
+    colors: {
+      main: "main-btn",
+      second: "secondary-btn",
+      icon: "white",
+      projectcolor: "#FFD5BD",
+    },
+  },
+  {
+    title: " Day Project",
+    description:'',
+    description_EN:'',
+    technologies: [
+      { name: "Html", icon: htmlicon },
+      { name: "CSS", icon: cssicon },
+      { name: "JavaScript", icon: javascripticon },
+      { name: "Bootstrap", icon: bootstarpicon },
+      { name: "React", icon: reacticon },
+      { name: "Figma", icon: figmaicon },
+    ],
+    image: Day,
+    deploymenturl: "https://secondproject-4d56f.web.app/",
+
+    githubicon: FiGithub,
+    deploymenticon: FiLink,
+    colors: {
+      main: "main-btn",
+      second: "secondary-btn",
+      icon: "white",
+      projectcolor: "#FFD5BD",
+    },
+  },
+
+  {
+    title: "Reve Group",
+    description:'',
+    description_EN:'',
+    technologies: [
+      { name: "Html", icon: htmlicon },
+      { name: "CSS", icon: cssicon },
+      { name: "JavaScript", icon: javascripticon },
+      { name: "React", icon: reacticon },
+      { name: "Next Js", icon: nextjsicon },
+      { name: "Axios", icon: axiosicon },
+      { name: "MongoDB", icon: mongodbicon },
+      { name: "Type Script", icon: typescripticon },
+      { name: "Node.js", icon: nodejsicon },
+      { name: "Figma", icon: figmaicon },
+
+    ],
+    image: instorMockup,
+    deploymenturl: "https://revegroups.com/",
+
+    githubicon: FiGithub,
+    deploymenticon: FiLink,
+    colors: {
+      main: "main-btn",
+      second: "secondary-btn",
+      icon: "white",
+      projectcolor: "#E3964A",
+    },
+  },
+  {
+    title: "Crud App",
+    description:'',
+    description_EN:'',
+    technologies: [
+      { name: "Html", icon: htmlicon },
+      { name: "CSS", icon: cssicon },
+      { name: "JavaScript", icon: javascripticon },
+      { name: "React", icon: reacticon },
+      { name: "Redux", icon: reduxixon },
+      { name: "Axios", icon: axiosicon },
+
+    ],
+    image: crud,
+    deploymenturl: "https://crud-app-ee322.web.app/",
+
+    githubicon: FiGithub,
+    deploymenticon: FiLink,
+    colors: {
+      main: "main-btn",
+      second: "secondary-btn",
+      icon: "white",
+      projectcolor: "#E3964A",
+    },
+  },
+  {
+    title: "Bootstrap App",
+    description:'',
+     description_EN:'',
+     technologies: [
+      { name: "Html", icon: htmlicon },
+      { name: "CSS", icon: cssicon },
+      { name: "JavaScript", icon: javascripticon },
+      { name: "Figma", icon: figmaicon },
+      { name: "bootstrap", icon: bootstarpicon },
+
+    ],
+    image: tastyMockup,
+    deploymenturl: "https://rayanhnide-fe1.github.io/Fitrst-Project/",
+
+    githubicon: FiGithub,
+    deploymenticon: FiLink,
+    colors: {
+      main: "main-btn",
+      second: "secondary-btn",
+      icon: "white",
+      projectcolor: "#70B9BE",
+    },
+  },
+
+
+] as const;
+
+export const liveTickerData = {
+  content: {
+    de: "Weitere Projekte auf Github",
+    en: "More Projects on Github",
+  },
+} as const;
+
+export const skillsDataWeb = [
+  {
+    skillsTitle: "WebDevelopment",
+    skills: [
+      {
+        title: "HTML",
+        hash: "#html",
+        icon: htmlicon,
+        color: "#F1662A",
+      },
+      {
+        title: "CSS",
+        hash: "#CSS",
+        icon: cssicon,
+        color: "#1572B6",
+      },
+      {
+        title: "JavaScript",
+        hash: "#JavaScript",
+        icon: javascripticon,
+        color: "#F7DF1E",
+      },
+      {
+        title: "TypeScript",
+        hash: "#TypeScript",
+        icon: typescripticon,
+        color: "#007ACC",
+      },
+      {
+        title: "React",
+        hash: "#React",
+        icon: reacticon,
+        color: "#61DAFB",
+      },
+      {
+        title: "Next.js",
+        hash: "#Next.js",
+        icon: [nextjsicon, nextjsiconwhite],
+        color: ["#000000", "#FFFFFF"],
+      },
+      {
+        title: "Node.js",
+        hash: "#Node.js",
+        icon: nodejsicon,
+        color: "#339933",
+      },
+      {
+        title: "Tailwind",
+        hash: "#Tailwind",
+        icon: tailwindcssicon,
+        color: "#38B2AC",
+      },
+
+
+      {
+        title: "SASS/SCSS",
+        hash: "#SASS/SCSS",
+        icon: sassscssicon,
+        color: "#CC6699",
+      },
+    ],
+  },
+] as const;
+
+export const skillsDataDesign = [
+  {
+    skillsTitle: "Design",
+    skills: [
+      { title: "Figma", hash: "#Figma", icon: figmaicon, color: "#F24E1E" },
+      {
+        title: "Adobe XD",
+        hash: "#Adobe XD",
+        icon: adobexdicon,
+        color: "#FF61F6",
+      },
+     ],
+  },
+] as const;
+
+export const skillsDataCMS = [
+  {
+    skillsTitle: "CMS",
+    skills: [
+
+
+      {
+        title: "Webflow",
+        hash: "#Webflow",
+        icon: webflowicon,
+        color: "#4353FF",
+      },
+    ],
+  },
+] as const;
+
+export const navLinks = [
+  { de: "Home", en: "Home", hash: "#home", icon: GoHome },
+  { de: "Skills", en: "Skills", hash: "#skills", icon: GoStack },
+  { de: "Projects", en: "Projects", hash: "#projects", icon: GoProject },
+  { de: "About me", en: "About me", hash: "#about-me", icon: GoPerson },
+  { de: "Contact", en: "Contact", hash: "#contact", icon: GoMail },
+] as const;
+
+export const FooterLinks = [
+  { de: "Impressum", en: "Imprint", hash: "#imprint", data: <Imprint /> },
+  { de: "Datenschutz", en: "Privacy", hash: "#privacy", data: <Privacy /> },
+] as const;
+
+export const sideBarRightMail = {
+  link: "eng.rayanhnide98@gmail.com",
+  text: "myemail@com",
+} as const;
+
+export const sideBarLeftSocials = [
+  {
+    link: "https://www.linkedin.com/in/rayan-hnide-53149a28a/",
+    icon: FiLinkedin,
+    altimgname: "linkedin",
+  },
+  {
+    link: "https://github.com/",
+    icon: FiGithub,
+    altimgname: "github",
+  },
+  {
+    link: "eng.rayanhnide98@gmail.com",
+    icon: FiMail,
+    altimgname: "mail",
+  },
+] as const;
+
+export const quotesData = [
+  {
+    de:" ليس من المهم على الإطلاق أن تقوم بالأمر بشكل صحيح في المرة الأولى. من المهم للغاية أن تقوم بذلك بشكل صحيح في المرة الأخيرة. ",
+     en: `"It's not at all important to get it right the first time. It's vitally important to get it right the last time."`,
+    author: "Andrew Hunt",
+  },
+  {
+    de:"الويب يشبه اللوحة القماشية، والرمز هو الطلاء. قم بإنشاء تحفتك الفنية.",
+     en: `"The web is like a canvas, and code is the paint. Create your masterpiece."`,
+  },
+] as const;
+
+export const aboutMeData = {
+  title: "About me",
+  title_EN: "About me",
+  description: "بعض مقتطفات التعليمات البرمجية عني",
+  description_EN: "A few code snippets about me",
+  paragraphs_DE: [
+    {
+      title: "Die Schaltkreise meines Herzens",
+      description:
+        "Wenn ich nicht in der digitalen Welt unterwegs bin, durchstreife ich die faszinierende Welt der Technik und Hardware. Schaltkreise und Lötstellen sind für mich wie Puzzleteile eines aufregenden Abenteuers.",
+      icon: hardwareicon,
+    },
+    {
+      title: "Auf der Überholspur des Lebens",
+      description:
+        "Neben dem Coden befinde ich mich gerne auf der Überholspur – im wahrsten Sinne des Wortes. Autos sind meine Leidenschaft, und ich genieße es, mit PS-starken Maschinen auf unbekannten Straßen unterwegs zu sein.",
+      icon: caricon,
+    },
+    {
+      title: "Entdeckungsfreude als Lebensmotto",
+      description:
+        "Meine Reise als Webentwickler ist nur ein Teil meines Lebenswegs. Ich lebe nach dem Motto, dass das Abenteuer erst beginnt, wenn man das Bekannte hinter sich lässt. Das Entdecken neuer Orte ist meine Form der kreativen Inspiration.",
+      icon: travelicon,
+    },
+  ],
+  paragraphs_EN: [
+    {
+      title: "The Circuits of My Heart",
+      description:
+        "When I'm not navigating the digital world, I explore the fascinating realm of technology and hardware. Circuits and solder joints are like puzzle pieces to me in an exciting adventure.",
+      icon: hardwareicon,
+    },
+    {
+      title: "On the Fast Lane of Life",
+      description:
+        "Besides coding, I like to be on the fast lane - in the truest sense of the word. Cars are my passion, and I enjoy being on unknown roads with powerful machines.",
+      icon: caricon,
+    },
+    {
+      title: "The Joy of Discovery as a Life Motto",
+      description:
+        "My journey as a web developer is only a part of my life path. I live by the motto that the adventure only begins when you leave the familiar behind. Discovering new places and cultures is my form of creative inspiration.",
+      icon: travelicon,
+    },
+  ],
+};
+
+export const contactData = {
+  title: {
+    de: "Kontakt",
+    en: "Contact",
+  },
+  description: {
+    de: "Schreib mir eine Nachricht und ich melde mich bei dir.",
+    en: "Write me a message and I will get back to you.",
+  },
+  inputfields: [
+    {
+      name: "name",
+      placeholder: {
+        de: "Dein Name",
+        en: "Your Name",
+      },
+      type: "text",
+      validation: {
+        de: "Bitte gebe deinen Namen ein",
+        en: "Please fill in your name",
+      },
+      pattern: "{2}",
+    },
+    {
+      name: "email",
+      placeholder: {
+        de: "Deine E-Mail Adresse",
+        en: "Your E-Mail",
+      },
+      type: "email",
+      validation: {
+        de: "Bitte gebe deine Email ein",
+        en: "Please fill in your email",
+      },
+      pattern: "[@]{4}",
+    },
+    {
+      name: "subject",
+      placeholder: {
+        de: "Deine Betreff",
+        en: "Your Subject",
+      },
+      type: "text",
+      validation: {
+        de: "Bitte gebe einen Betreff ein",
+        en: "Please fill in your subject",
+      },
+      pattern: "{10}",
+    },
+  ],
+  textarea: {
+    placeholder: {
+      de: "Deine Nachricht",
+      en: "Your Message",
+    },
+    name: "message",
+    rows: 10,
+    validation: {
+      de: "Bitte gebe deine Nachricht ein",
+      en: "Please fill in your message",
+    },
+    pattern: "{10}",
+  },
+  button: {
+    value: {
+      de: "Absenden",
+      en: "Send",
+    },
+  },
+  icon: FiMail,
+  iconcolor: "#FFFFFF",
+  colors: {
+    main: "main-btn",
+    second: "secondary-btn",
+    icon: "white",
+  },
+  privacyOptIn: {
+    checkbox: {
+      de: "Ich stimme zu, dass Alpay meine personenbezogenen Daten (Name und E-Mail-Adresse) verwenden darf, um mit mir Kontakt aufzunehmen.",
+      en: "I agree that Alpay may use my personal data (name and e-mail address) to contact me.",
+    },
+    description: {
+      de: "Durch Übermittlung dieser Anfrage bestätigen Sie, dass Sie die Datenschutzerklärung gelesen haben",
+      en: "By submitting this request, you acknowledge that you have read the Private Policy",
+    },
+  },
+} as const;
+
+export const toastMessages = {
+  loadingProject: {
+    de: "🦄 Die Live Demo wird gleich geöffnet. Server werden gestartet...",
+    en: "🦄 The live demo will open shortly. Starting servers...",
+  },
+  successEmailSent: {
+    de: "🦄 Vielen Dank für deine Email. Ich werde mich schnellstmöglich bei dir melden",
+    en: "🦄 Thank you for your email. I will get back to you as soon as possible",
+  },
+  failedEmailSent: {
+    de: "🦄 Leider hat der Versand deiner Email nicht geklappt. Bitte versuche es später noch einmal",
+    en: "🦄 Unfortunately the sending of your email did not work. Please try again later",
+  },
+  failedValidationName: {
+    de: "Bitte gebe deinen Namen ein",
+    en: "Please fill in your name",
+  },
+} as const;
+
+export const buttonLabels = {
+  language: {
+    de: "DE",
+    en: "EN",
+  },
+} as const;
+
+export const directionStyles: Record<string, React.CSSProperties> = {
+  "outer-right-to-inner-left": {
+    marginRight: "4rem",
+  },
+  "outer-left-to-inner-right": {
+    marginLeft: "4rem",
+    transform: "scaleX(-1)",
+  },
+  "inner-right-to-middle": {
+    width: "100%",
+    transform: "scaleY(1)",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  "inner-left-to-middle": {
+    width: "100%",
+    transform: "scaleX(-1)",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    overflow: "hidden",
+  },
+  "middle-to-inner-right": {
+    width: "100%",
+    transform: "scale(1,-1)",
+  },
+  "middle-to-inner-left": {
+    width: "100%",
+    transform: "scale(-1,-1)",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  middle: {
+    width: "100%",
+    transform: "scaleX(-1)",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+};
+
+export const heightStyles: Record<string, { heights: [string, string] }> = {
+  small: {
+    heights: ["25rem", "15rem"],
+  },
+  middle: {
+    heights: ["35rem", "25rem"],
+  },
+  large: {
+    heights: ["45rem", "35rem"],
+  },
+  extraLarge: {
+    heights: ["55rem", "45rem"],
+  },
+};
+
+export const spaceStyles: Record<string, React.CSSProperties> = {
+  "outer-right-to-inner-left": {
+    marginTop: "-6rem",
+    width: "100%",
+  },
+  "outer-left-to-inner-right": {
+    marginTop: "-6rem",
+    width: "100%",
+  },
+  "inner-right-to-middle": {
+    marginTop: "-20rem",
+    width: "50%",
+    zIndex: "-10",
+  },
+  "inner-left-to-middle": {
+    marginTop: "-10rem",
+    width: "50%",
+    zIndex: "-10",
+  },
+  "middle-to-inner-right": {
+    width: "75%",
+  },
+  "middle-to-inner-left": {
+    marginTop: "-2.5rem",
+    width: "50%",
+  },
+  middle: {
+    marginTop: "-2.5rem",
+    width: "0%",
+    display: "none",
+  },
+};
+
+export const widthStyles: Record<string, { widths: [string, string] }> = {
+  "outer-right-to-inner-left": {
+    widths: ["74.45%", "74.45%"],
+  },
+  "outer-left-to-inner-right": {
+    widths: ["75%", "75%"],
+  },
+  "inner-right-to-middle": {
+    widths: ["50.1%", "49%"],
+  },
+  "inner-left-to-middle": {
+    widths: ["50.1%", "49%"],
+  },
+  "middle-to-inner-right": {
+    widths: ["33.4%", "50.03%"],
+  },
+  "middle-to-inner-left": {
+    widths: ["50.1%", "49%"],
+  },
+  middle: {
+    widths: ["0%", "0%"],
+  },
+};
